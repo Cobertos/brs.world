4c581abbc3c97c71efa5f6bc4738fa720879c557
Cobertos / Peter <me+git@cobertos.com>
2019-09-26 00:30:50 -0400

    feat(scss): Tons of interactive styles to the sidebar

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import "scss/main.scss";
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/BRSUI.vue
+++ src/components/BRSUI.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="brs-container">
     <BRSViewer :brsBuff="brsBuff" />
-    <div class="brs-main">
+    <div class="brs-main"
+      :class="{'keep-open': interacting}"
+      @mouseenter="interacting = true"
+      @mouseleave="stopInteracting">
+      <div class="brs-header">
+        <h2>BRS.world</h2>
+        <p>View and share your Brickadia builds</p>
+        <button
+          @click="openFileUploader">Open .brs</button>
+        <p v-if="file">
+          {{file.name}}{{file.size}}
+          <button @click="uploadFile">Upload</button>
+        </p>
+        <input type="file"
+          ref="fileUpload"
+          style="display:none"
+          @change="onFileChange($event)" />
+      </div>
       <div class="brs-list">
-        <div class="brs-list-item">
-          <input type="file"
-            @change="onFileChange($event)" />
-        </div>
         <div class="brs-list-item"
-          v-for="brsInfo in brsInfoList"
+          v-for="brsInfo in brsInfoWState"
           :key="brsInfo.url"
-          @click="onEntryClick($event, brsInfo.url)">
+          @click="onEntryClick($event, brsInfo)"
+          :class="{selected: brsInfo.selected}">
           <h2>{{brsInfo.map}}</h2>
           <p>{{brsInfo.author.name}}</p>
-          <p>{{brsInfo.description}}</p>
+          <p v-if="false">{{brsInfo.description}}</p>
         </div>
       </div>
     </div>
@@ -32,6 +46,8 @@ export default {
   data(){ 
     return {
       brsBuff: undefined,
+      interacting: false,
+      uploadFile: undefined,
       brsInfoList: [{
         map: "Bowling Alley",
         author: {id: "xxx", name: "Cobertos"},
@@ -93,7 +109,22 @@ export default {
         url: "https://s3.us-east-2.amazonaws.com/cobstatic.com/brs.world/builds/myChallengeOnHatasServer.brs"
         //save_time: UTC as 8 bytes,
         //brick_count: int,
+      },{
+        map: "DONOTUSE Stress Test - 14 mil",
+        author: {id: "xxx", name: "Kodi"},
+        description: "Stress test",
+        url: "https://s3.us-east-2.amazonaws.com/cobstatic.com/brs.world/builds/spam+1x1f.brs"
       }]
+    }
+  },
+  computed: {
+    brsInfoWState(){
+      return this.brsInfoList.map((obj)=>{
+        return {
+          selected: false,
+          ...obj
+        };
+      });
     }
   },
   methods:{
@@ -103,71 +134,127 @@ export default {
       if(!file) {
         return;
       }
+      this.file = noObserve(file);
       console.log("Start file convert...");
       this.brsBuff = await file.arrayBuffer();
     },
-    async onEntryClick(e, url){
+    async onEntryClick(e, brsInfo){
       console.log(e);
       if(e.target.tagName === "input") {
         return;
       }
       //Load the BRSHMesh
+      this.brsInfoWState.forEach((brsInfo)=>brsInfo.selected = false);
+      brsInfo.selected = true;
       console.log("Start download...");
-      const brsObj = await fetch(url);
+      const brsObj = await fetch(brsInfo.url);
       this.brsBuff = await brsObj.arrayBuffer();
+    },
+    stopInteracting(){
+      setTimeout(()=>this.interacting=false, 500);
+    },
+    openFileUploader(){
+      this.$refs.fileUpload.click();
     }
   }
 }
 </script>
 
 <style lang="scss">
+@import "../scss/_func.scss";
+
 .brs-main {
   position: absolute;
-  left: 0;
+  left: -280px;
   top: 0;
   height: 100vh;
-  overflow: auto;
   width: 300px;
   color: white;
+  transition: left 0.2s;
+  background-color: #333333;
 
-  .brs-list-item {
-    min-height: 80px;
-    text-align: left;
-    padding: 20px;
-
-
-    &:hover {
-      background-image: linear-gradient(to left, #FFFFFF 0%, #AAAAAA 100%)
-    }
+  &:hover, &.keep-open {
+    left: 0;
   }
 
-  //Scrollbar webkit
-  &::-webkit-scrollbar {
-      width: 12px;
-      height: 12px;
+  .brs-list{
+    overflow-y: auto;
+    user-select: none;
+    .brs-list-item {
+      min-height: 40px;
+      text-align: left;
+      padding: 20px;
+      //Interesting chevron effect
+      /*background-image: 
+          linear-gradient(100deg, rgba(0,0,0,0) 20px, #112222 20px, #112222 40px, rgba(0,0,0,0) 40px),
+          linear-gradient(100deg, rgba(0,0,0,0) 60px, #112222 60px, #112222 80px, rgba(0,0,0,0) 80px),
+          linear-gradient(100deg, rgba(0,0,0,0) 100px, #112222 100px, #112222 120px, rgba(0,0,0,0) 120px),
+          linear-gradient(100deg, rgba(0,0,0,0) 140px, #112222 140px);*/
+      background-image: 
+        linear-gradient(to right, #CC4444 50%, #333333 50%);
+      background-position: 100% 0;
+      background-size: 200% 200%;
+      background-repeat: no-repeat;
+      transition: background-position 0.2s, background-color 0.2s;
+      cursor: pointer;
 
-      /*@include desktop {
-          width: 10px;
-          height: 10px;
-      }*/
-  }
-  &::-webkit-scrollbar-button {
-      display: none; //No up or down buttons
-  }
-  &::-webkit-scrollbar-thumb {
-      background-color: #AAAAAA;
-      border-radius: 9999px;
       &:hover {
-          background-color: #FFFFFF;
+        background-position: 70% 0;
       }
-  }
-  &::-webkit-scrollbar-track {
-      display: none;
-  }
-  //Scrollbar IE and FF
-  scrollbar-color: #AAAAAA rgba(0,0,0,0); //thumb track
-  &:hover {
-      scrollbar-color: #FFFFFF rgba(0,0,0,0); //thumb track
+      &:active, &.selected {
+        background-position: 0 0;
+      }
+      &.selected {
+        animation: selected-fade 2s;
+        background-image: none;
+        background-color: #CC4444;
+      }
+
+      @keyframes selected-fade {
+        from {
+          background-color: white;
+        }
+        to {
+          background-color: #CC4444;
+        }
+      }
+
+      h2 {
+        margin: 0;
+      }
+      p {
+        margin: 0;
+      }
+    }
+
+    //Scrollbar webkit
+    &::-webkit-scrollbar {
+        width: 12px;
+        height: 12px;
+
+        /*@include desktop {
+            width: 10px;
+            height: 10px;
+        }*/
+    }
+    &::-webkit-scrollbar-button {
+        display: none; //No up or down buttons
+    }
+    &::-webkit-scrollbar-thumb {
+        background-color: #AAAAAA;
+        &:hover {
+            background-color: #FFFFFF;
+            cursor: pointer;
+        }
+    }
+    &::-webkit-scrollbar-track {
+        background-color: #333333;
+    }
+    //Scrollbar IE and FF
+    scrollbar-color: #AAAAAA #333333; //thumb track
+    &:hover {
+        scrollbar-color: #FFFFFF #333333; //thumb track
+    }
   }
 }
 </style>
